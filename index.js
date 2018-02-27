f920f33884aa19a8b31c07f5198705c0b5369369
sleeplesseditor <josh@bryson.me.uk>
2018-02-27 09:57:58 +0000

    Table function established

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 const Table = require('cli-table');
 
-let users = [];
+let table = new Table({
+    head: ['username', '❤️', 'challenges'],
+    colWidths: [15, 5, 10]
+})
 
 const options = {
     url: `https://forum.freecodecamp.org/directory_items?period=week&order=likes_received&_=1518604435748`,
@@ -23,5 +26,30 @@ rp(options)
     });
 
     function getChallengesCompletedAndPushToUserArray(userData) {
-        
+        var i = 0;
+        function next() {
+            if (i < userData.length) {
+                var options = {
+                    url: `https://www.freecodecamp.org/` + userData[i].name,
+                    transform: body => cheerio.load(body)
+                }
+                rp(options)
+                    .then(function ($) {
+                        process.stdout.write(`.`);
+                        const fccAccount = $('h1.landing-heading').length == 0;
+                        const challengesPassed = fccAccount ? $('tbody tr').length : 'unknown';
+                        table.push([userData[i].name, userData[i].likes_received, challengesPassed]);
+                        ++i;
+                        return next;
+                    })
+            } else {
+                printData();
+            }
+        }
+        return next();
+    };
+
+    function printData() {
+        console.log("✅");
+        console.log(table.toString());
     }
